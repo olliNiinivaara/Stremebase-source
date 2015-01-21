16feb53371e047b178401c3f51244fd0d315bd95
olli niinivaara <olli.niinivaara@gmail.com>
2015-01-21 13:44:32 +0200

    jar + tutorials

---
+++ Tutorials/src/com/stremebase/tutorials/basic/Chapter_1_Introduction.java
@@ -0,0 +1,45 @@
+package com.stremebase.tutorials.basic;
+
+public class Chapter_1_Introduction
+{
+
+  public static void main(String[] args)
+  {
+    welcome();
+    lesson1_startingTheDB();
+    lesson2_creatingAMap();
+    lesson3_crud();
+    lesson4_query();
+    lesson5_closingwords();
+  }
+  
+  public static void welcome()
+  {
+    
+  }
+  
+  public static void lesson1_startingTheDB()
+  {
+    
+  }
+  
+  public static void lesson2_creatingAMap()
+  {
+    
+  }
+  
+  public static void lesson3_crud()
+  {
+    
+  }
+  
+  public static void lesson4_query()
+  {
+    
+  }
+  
+  public static void lesson5_closingwords()
+  {
+    
+  }  
+}
